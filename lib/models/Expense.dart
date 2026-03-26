ce866bd78ba38ec0aba0ae1d68b7722ae080b0cf
sdhingra5 <sidaqdhingra4@gmail.com>
2026-03-26 11:46:32 -0400

    made dummy data in expenses

--- lib/models/Expense.dart
+++ lib/models/Expense.dart
@@ -1,11 +1,16 @@
 import 'package:uuid/uuid.dart';
-const uuid = Uuid();
+const uuid = Uuid();//making the uuid class
+
+enum Category {leisure,food,travel,work}
+
 class Expense {
-  Expense ({required this.title, required this.amount, required this.date}) : id = uuid.v4(); 
+  Expense ({required this.title, required this.amount, 
+  required this.date, required this.category}) : id = uuid.v4(); 
 
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category; //to flexible -> limits this to 4 cats -> leisure, food, travel, work
 }
