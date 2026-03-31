b9f48d93fb1f3d56063fbd65b69b86c9d1743552
sdhingra5 <sidaqdhingra4@gmail.com>
2026-03-31 11:30:33 -0400

    added in starting card to expense item and loaded in expense list

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/widgets/expenses_list/expense_Item.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/Expense.dart';
 
@@ -9,7 +10,8 @@ class ExpensesList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount:expenses.length,
-      itemBuilder: (ctx, index) => Text(expenses[index].title),
+      itemBuilder: (ctx, index) => 
+      ExpenseItem(expense: expenses[index]),
     );
   }
 }
