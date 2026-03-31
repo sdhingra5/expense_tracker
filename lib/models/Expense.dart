86f95c396943285867cf4e0a6fb4d989095823c4
sdhingra5 <sidaqdhingra4@gmail.com>
2026-03-31 12:09:08 -0400

    show correct icon and formatted date on the card

--- lib/models/Expense.dart
+++ lib/models/Expense.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 const uuid = Uuid();//making the uuid class
+final formatter = DateFormat.yMd();
+enum Category {food,travel,leisure,work}
+const categoryIcons ={
+  Category.food:Icons.lunch_dining,
+  Category.travel:Icons.flight_takeoff,
+  Category.leisure:Icons.movie,
+  Category.work:Icons.work,
 
-enum Category {leisure,food,travel,work}
+};
 
 class Expense {
   Expense ({required this.title, required this.amount, 
@@ -13,4 +22,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category; //to flexible -> limits this to 4 cats -> leisure, food, travel, work
+
+  String get formattedDate{
+    return formatter.format(date);
+  }
 }
--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -17,14 +17,13 @@ class ExpenseItem extends StatelessWidget {
           children: [
             Text(expense.title), //Text box at the top of the col
             const SizedBox(height: 4,),//a little space between first row of the col and the second
-            Row( 
-              children: [
-                Text('\$${expense.amount.toStringAsFixed(2)}'),
-                Spacer(),
-                Row(children: [
-                  const Icon(Icons.alarm),
+            Row( children: [ // We need a bunch of things in this row of the col
+                Text('\$${expense.amount.toStringAsFixed(2)}'), //amount first
+                Spacer(),//spacer to push the date to the right
+                Row(children: [ //Cat and date closely grouped, so another row in this row
+                  Icon(categoryIcons[expense.category]),//chamge this later to look up a correct icon for the cat
                   const SizedBox(width:8),
-                  Text(expense.date.toString()),
+                  Text(expense.formattedDate), //fix this date to not look stupid
                 ],)
                
               ],),
